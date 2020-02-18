0333438fe56909b178cf955f24027ff66f9be614
wangcaiwen5 <568616550@qq.com>
2020-02-18 16:00:17 +0800

    组件化项目部署

--- ordermodule/src/main/java/com/cw/ordermodule/MainActivity.kt
+++ ordermodule/src/main/java/com/cw/ordermodule/MainActivity.kt
@@ -3,6 +3,9 @@ package com.cw.ordermodule
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * 测试
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
